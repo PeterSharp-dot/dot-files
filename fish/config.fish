2b960b041c1b12d72f461996e869592a07aaf731
Piotr Grela <peter.grela@gmail.com>
2023-05-19 20:05:05 +0200

    ok. po raz drugi.

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,33 @@
-/home/peter/.config/fish/config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fish_add_path /home/peter/.bin 
+fish_add_path /home/peter/.local/bin 
+fish_add_path /home/peter/.cargo/bin
+fish_add_path /home/peter/.local/lib
+set -gx COOKIE_FILE "/home/peter/.local/bin/cookies.json"
+set -gx TERM xterm-kitty
+set -gx GPG_TTY $(tty)
+set -gx MANWIDTH 80
+set -gx STARDICT_DATA_DIR "/home/peter/sdcv/"
+set -gx TERMINAL "/usr/bin/kitty"
+set -gx BROWSER firefox
+set -gx COLORTERM truecolor
+set -gx SHFM_OPENER "/home/peter/.local/bin/shfm-opener.sh"
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx FZF_DEFAULT_COMMAND 'ag --hidden --depth 4 --ignore .git -f -g ""'
+set -gx BAT_THEME base16
+set -gx XDG_CURRENT_DESKTOP KDE
+set -gx QT_QPA_PLATFORMTHEME qt5ct
+
+set -g fish_greeting 'Welcome to Fish shell  '
+function fish_prompt 
+  printf '>'
+end
+function fish_right_prompt 
+  pwd 
+end
+function t
+  clear && pwd && tree -L 1
+end
