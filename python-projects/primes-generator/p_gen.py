e5aa930728d2bd7fc04c4e71c198b9f3fc66d3e3
Piotr <peter.grela@gmail.com>
2023-06-18 09:21:11 +0200

    ok

--- python-projects/primes-generator/p_gen.py
+++ python-projects/primes-generator/p_gen.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 
 def primesGen(max):
-    primes = [3]
+    primes = []
     l = 3
-    # import pdb; pdb.set_trace()   
     while l <= max:
-        for i in primes:
-            if (l % 2 == 1 and l % i == 0 and l == i):
-                     primes.append(l)
-                     l += 1
-            else: 
-                l += 1
-                primes.append(l)
+        if  (l % 2 == 1):  
+            primes.append(l)
+        l += 1
 
-        # for i in primes:
-            # if ( l % 2 == 1 and l % i == 0 and i != l):  
-                # primes.append(l)
-
+    # import pdb; pdb.set_trace()   
+    for i in primes:
+        if i > 3:
+            for j in primes:
+                if (i > j) and (i % j == 0):
+                    primes.remove(i)
+                    break
+    for i in primes:
+        if i > 3:
+            for j in primes:
+                if (i > j) and (i % j == 0):
+                    primes.remove(i)
+                    break
 
 
     primes.insert(0,2)
     primes.insert(0,1)
-    # print(set(primes))
     print(primes)
         
 
