5efc222b2c091b8dc51d5692758754b4c6e446de
Piotr Grela <peter.grela@gmail.com>
2024-07-11 16:39:25 +0200

    ok

--- lazy-nvim/nvim/lua/config/options.lua
+++ lazy-nvim/nvim/lua/config/options.lua
@@ -1,10 +1,48 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local function set_highlight_normal()
+  vim.cmd("hi Normal guifg=#8288a1")
+end
+vim.defer_fn(function()
+  set_highlight_normal()
+end, 0)
 vim.o.scrolloff = 0 -- one text line of previous screen
-vim.o.autochdir = true
+-- vim.o.autochdir = false
 -- vim.o.textwidth = 80
 vim.o.colorcolumn = "80"
-vim.o.wrap = true
-vim.o.linebreak = true
-vim.o.relativenumber = true
+vim.wo.wrap = true
+vim.wo.linebreak = true
+vim.o.relativenumber = false
+vim.o.number = true
+vim.opt.conceallevel = 1
+vim.cmd("syntax enable")
+vim.opt.tags = { "/home/peter/notes/tags" }
+
+-- Funkcja do otwierania istniejącego pliku lub tworzenia nowego
+function Go_to_file_or_create()
+  -- Pobierz nazwę pliku spod kursora
+  local file_name = vim.fn.expand("<cfile>")
+
+  -- Sprawdź, czy plik istnieje
+  local file_exists = vim.fn.filereadable(file_name)
+
+  -- Jeśli plik istnieje, otwórz go w nowym buforze
+  if file_exists == 1 then
+    vim.cmd("edit " .. file_name)
+  else
+    -- Jeśli plik nie istnieje, utwórz nowy plik
+    vim.cmd("edit " .. file_name)
+    print("New file created: " .. file_name)
+  end
+  -- else
+  --   -- Jeśli plik nie istnieje, utwórz nowy plik
+  --   local create_command = "edit " .. file_name
+  --   vim.fn.system(create_command)
+  --   print("New file created: " .. file_name)
+  -- end
+end
+
+-- Zmapuj gf bezpośrednio do polecenia, które wywoła funkcję Lua
+vim.api.nvim_set_keymap("n", "gf", ":lua Go_to_file_or_create()<CR>", { noremap = true, silent = true })
