96e291250ce3e61d704a67b1b4265efe9afc171a
Piotr Grela <peter.grela@gmail.com>
2024-07-12 19:02:23 +0200

    ok

--- lazy-nvim/nvim/lua/config/laptop-options.lua
+++ lazy-nvim/nvim/lua/config/laptop-options.lua
@@ -34,15 +34,36 @@ local function is_git_repo()
   end
   return result:match("true") ~= nil
 end
+
+-- Funkcja sprawdzająca, czy ścieżka zawiera określony katalog
+local function is_in_directory(file_path, dir_name)
+  return file_path:find("/" .. dir_name .. "/") ~= nil
+end
+
 -- Definiowanie funkcji, która wykonuje git add i git commit
 local function git_add_and_commit()
-  if is_git_repo() then
-    os.execute("git add . > /dev/null 2>&1")
-    os.execute("git commit -m 'ok' > /dev/null 2>&1")
-    os.execute("git push > /dev/null 2>&1")
-    print("git commit has been pushed")
+  local file_path = vim.fn.expand("%:p") -- Pełna ścieżka aktualnego pliku
+  local dir_name
+
+  if is_in_directory(file_path, "notes") then
+    dir_name = "notes"
+  elseif is_in_directory(file_path, "dot-files") then
+    dir_name = "dot-files"
+  end
+
+  if dir_name then
+    if is_git_repo() then
+      local git_command = "cd "
+        .. dir_name
+        .. " && git add . > /dev/null 2>&1 && git commit -m 'ok' > /dev/null 2>&1 && git push > /dev/null 2>&1"
+      os.execute(git_command)
+      print("Git commit has been pushed in " .. dir_name)
+    end
+  else
+    print("File is not in 'notes' or 'dot-files' directory.")
   end
 end
+
 -- Dodawanie autokomentarzy
 vim.api.nvim_create_augroup("AutoGitCommit", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePost", {
@@ -50,6 +71,37 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*",
   callback = git_add_and_commit,
 })
+
+------------------------------------------------------------------------
+-- -- Definiowanie funkcji, która sprawdzi, czy plik jest w repozytorium Git
+-- local function is_git_repo()
+--   local handle = io.popen("git rev-parse --is-inside-work-tree 2>/dev/null")
+--   if handle == nil then
+--     return false
+--   end
+--   local result = handle:read("*a")
+--   handle:close()
+--   if result == nil then
+--     return false
+--   end
+--   return result:match("true") ~= nil
+-- end
+-- -- Definiowanie funkcji, która wykonuje git add i git commit
+-- local function git_add_and_commit()
+--   if is_git_repo() then
+--     os.execute("git add . > /dev/null 2>&1")
+--     os.execute("git commit -m 'ok' > /dev/null 2>&1")
+--     os.execute("git push > /dev/null 2>&1")
+--     print("git commit has been pushed")
+--   end
+-- end
+-- -- Dodawanie autokomentarzy
+-- vim.api.nvim_create_augroup("AutoGitCommit", { clear = true })
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   group = "AutoGitCommit",
+--   pattern = "*",
+--   callback = git_add_and_commit,
+-- })
 
 -- Funkcja do otwierania istniejącego pliku lub tworzenia nowego
 function Go_to_file_or_create()
