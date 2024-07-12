45ada01142b092ebb646868430f5b53b89e78f09
Piotr <peter.grela@gmail.com>
2024-07-12 16:09:36 +0200

    ok

--- lazy-nvim/nvim/lua/plugins/disabled.lua
+++ lazy-nvim/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "echasnovski/mini.surround", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
+  -- { "rcarriga/nvim-notify", enabled = false },
   { "folke/noice.nvim", enabled = false },
 }
