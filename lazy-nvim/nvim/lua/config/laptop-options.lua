b1c8c0ed3198ebb9fc0db510f5ea7bac5d10c61e
Piotr Grela <peter.grela@gmail.com>
2024-07-19 10:29:58 +0200

    ok

--- lazy-nvim/nvim/lua/config/laptop-options.lua
+++ lazy-nvim/nvim/lua/config/laptop-options.lua
@@ -2,13 +2,97 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+--Funkcja ustawiająca colorscheme i colorcolumn w TTY
+-- local function set_tty_colorscheme()
+--   local status1, err1 = pcall(function()
+--     vim.cmd("colorscheme vim")
+--   end)
+-- if not status1 then
+--   print("Error setting colorscheme:", err1)
+-- else
+--   print("Colorscheme set to vim")
+-- end
+
+--   local status2, err2 = pcall(function()
+--     vim.cmd("set colorcolumn=")
+--   end)
+--   if not status2 then
+--     print("Error setting colorcolumn:", err2)
+--   else
+--     print("Colorcolumn set to empty")
+--   end
+-- end
+
+-- Sprawdzenie czy Neovim działa w trybie tekstowym (TTY)
+-- if not os.getenv("DISPLAY") then
+--   vim.defer_fn(function()
+--     set_tty_colorscheme()
+--   end, 0)
+-- end
+
+-- vim.cmd("highlight Normal guifg=#DDDDDD")
+-- vim.cmd("colorscheme vim")
+--
+local function set_syntax_on()
+  vim.cmd("syntax on")
+  -- vim.opt.colorcolumn = ""
+end
+-- Tworzenie autokomendy
+vim.api.nvim_create_autocmd("BufRead", {
+  pattern = "*.leb",
+  callback = set_syntax_on,
+})
+
+-- bg=#10131c
 local function set_highlight_normal()
-  vim.cmd("hi Normal guifg=#8288a1")
+  vim.cmd("hi Normal ctermbg=black ctermfg=gray guibg=#0e0e0f guifg=#adb3c7")
 end
 vim.defer_fn(function()
   set_highlight_normal()
 end, 0)
 
+-- Funkcja do sprawdzenia, czy bufor jest częścią repozytorium Git
+local function is_git_repo(file)
+  local cmd = "git -C " .. file .. " rev-parse --is-inside-work-tree 2>/dev/null"
+  local handle = io.popen(cmd)
+  if handle == nil then
+    print("something's wrong")
+  else
+    local result = handle:read("*a")
+    handle:close()
+    return result:find("true") ~= nil
+  end
+end
+
+-- Funkcja do sprawdzenia, czy plik został zmieniony
+local function is_file_modified(file)
+  local cmd = "git -C " .. file .. " status --porcelain 2>/dev/null"
+  local handle = io.popen(cmd)
+  if handle == nil then
+    print("something's wrong")
+  else
+    local result = handle:read("*a")
+    handle:close()
+    return result ~= ""
+  end
+end
+
+-- Funkcja powiadomienia przy zamknięciu Neovim
+local function notify_on_exit()
+  -- Pobierz ścieżkę do bieżącego bufora
+  local file = vim.fn.expand("%:p:h")
+  if is_git_repo(file) and is_file_modified(file) then
+    os.execute('notify-send "Neovim" "Plik z repozytorium Git został zmieniony!"')
+  end
+end
+
+-- Ustawienie autokomendy, która uruchamia funkcję notify_on_exit przy zamknięciu Neovim
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  callback = notify_on_exit,
+})
+
+vim.api.nvim_command("syntax on")
+-- vim.cmd("syntax on")
 vim.o.scrolloff = 0 -- one text line of previous screen
 vim.o.autochdir = true
 -- vim.o.textwidth = 80
@@ -18,36 +102,10 @@ vim.wo.linebreak = true
 vim.o.relativenumber = false
 vim.o.number = true
 vim.opt.conceallevel = 1
-vim.cmd("syntax enable")
 vim.opt.tags = { "/home/peter/notes/tags" }
 
--- Funkcja do sprawdzenia, czy bufor jest częścią repozytorium Git
-local function is_git_repo()
-  local handle = io.popen("git rev-parse --is-inside-work-tree 2>/dev/null")
-  local result = handle:read("*a")
-  handle:close()
-  return result:find("true") ~= nil
-end
-
--- Funkcja do sprawdzenia, czy plik został zmieniony
-local function is_file_modified()
-  local handle = io.popen("git status --porcelain 2>/dev/null")
-  local result = handle:read("*a")
-  handle:close()
-  return result ~= ""
-end
-
--- Funkcja powiadomienia przy zamknięciu Neovim
-local function notify_on_exit()
-  if is_git_repo() and is_file_modified() then
-    os.execute('notify-send "Neovim" "Plik z repozytorium Git został zmieniony!"')
-  end
-end
-
--- Ustawienie autokomendy, która uruchamia funkcję notify_on_exit przy zamknięciu Neovim
-vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = notify_on_exit,
-})
+-- Po 5 sekundach ukryj komunikaty w command-line
+vim.api.nvim_command("autocmd CmdlineLeave * lua vim.defer_fn(function() vim.cmd(\"echo ''\") end, 5000)")
 
 -- Funkcja do otwierania istniejącego pliku lub tworzenia nowego
 function Go_to_file_or_create()
@@ -60,6 +118,7 @@ function Go_to_file_or_create()
   -- Jeśli plik istnieje, otwórz go w nowym buforze
   if file_exists == 1 then
     vim.cmd("edit " .. file_name)
+    print("Oto istniejący plik:" .. file_name)
   else
     -- Jeśli plik nie istnieje, utwórz nowy plik
     vim.cmd("edit " .. file_name)
@@ -69,6 +128,3 @@ end
 
 -- Zmapuj gf bezpośrednio do polecenia, które wywoła funkcję Lua
 vim.api.nvim_set_keymap("n", "gf", ":lua Go_to_file_or_create()<CR>", { noremap = true, silent = true })
-
--- Po 5 sekundach ukryj komunikaty w command-line
-vim.api.nvim_command("autocmd CmdlineLeave * lua vim.defer_fn(function() vim.cmd(\"echo ''\") end, 5000)")
