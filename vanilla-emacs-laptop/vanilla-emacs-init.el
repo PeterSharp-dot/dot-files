d420b4b09a680296ef79aef621449556df7b14f5
Piotr Grela <peter.grela@gmail.com>
2024-07-24 10:49:51 +0200

    ok

--- vanilla-emacs-laptop/vanilla-emacs-init.el
+++ vanilla-emacs-laptop/vanilla-emacs-init.el
@@ -11,42 +11,40 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(set-frame-font "Inconsolata LGC Nerd Font-13" nil t)
-(set-face-attribute 'default nil :font "Inconsolata LGC Nerd Font-13")
-(set-face-attribute 'fixed-pitch nil :font "Inconsolata LGC Nerd Font-15")
-(set-face-attribute 'variable-pitch nil :font "Inconsolata LGC Nerd Font-13")
+(set-frame-font "Inconsolata LGC Nerd Font-10.5" nil t)
+(set-face-attribute 'default nil :font "Inconsolata LGC Nerd Font-10.5")
+(set-face-attribute 'fixed-pitch nil :font "Inconsolata LGC Nerd Font-10")
+(set-face-attribute 'variable-pitch nil :font "Inconsolata LGC Nerd Font-10.5")
 
 ;; Inicjalizacja pakietów
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-
-;; Automatyczna instalacja use-package jeśli nie jest zainstalowany
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
-;; Przykład konfiguracji dla pakietu `ivy`
-
-;; (use-package ivy
-;;   :ensure t
-;;   :diminish
-;;   :bind (("C-s" . swiper))
-;;   :config
-;;   (ivy-mode 1))
-;; Składnia use-package
-;; :ensure t: Automatycznie instaluje pakiet, jeśli nie jest zainstalowany.
-;; :diminish: Usuwa nazwę pakietu z modeline, aby zachować porządek.
-;; :bind: Przypisuje klawisze do poleceń pakietu.
-;; :config: Sekcja, w której można umieścić kod konfigurujący pakiet po jego załadowaniu.
-;; :init: Kod, który ma być uruchomiony przed załadowaniem pakietu.
-;; :hook: Automatycznie dodaje funkcje do odpowiednich hooków.
-
-
-;;M-x load-file RET ~/.vanilla-emacs/vanilla-emacs-init.el RET		
+;; package-refresh-contents
 
 (use-package markdown-mode
   :ensure t
+  :config
+    (autoload 'markdown-mode "markdown-mode"
+      "Major mode for editing Markdown files" t)
+    (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+    (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+    (add-to-list 'auto-mode-alist '("\\.mkd\\'" . markdown-mode))
+    (add-to-list 'auto-mode-alist '("\\.mdown\\'" . markdown-mode))
+    (add-to-list 'auto-mode-alist '("\\.mkdn\\'" . markdown-mode))
+    :hook
+         ((markdown-mode . flyspell-mode)
+          (markdown-mode . visual-line-mode))
 )
+
+(use-package flycheck
+  :ensure t
+  :hook (after-init . global-flycheck-mode)
+)
+
+(use-package dot-mode
+  :ensure t
+  :hook (find-file . dot-mode-on)
+  :bind
+      ("C-." . dot-mode-execute)
+      ("C-M-." . dot-mode-override))
