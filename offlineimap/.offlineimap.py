1e29efa9d881fef72806ed2d481a9c94732e7161
Piotr Grela <peter.grela@gmail.com>
2024-10-17 20:23:31 +0200

    ok

--- offlineimap/.offlineimap.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# this is a script to retrieve pass  from pass
-from subprocess import check_output
-
-
-def get_pass(account):
-    return check_output("pass " + account, shell=True).splitlines()[0]
